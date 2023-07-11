233e006328f6f3c7c962e118ee280018307d9cae
AnirudhA1244 <agrawalanirudh18@gmail.com>
2023-07-11 20:51:47 +0530

    integrated virtual mouse. and converted all buttons to submit button.

--- Air-Glide(WebApp)/app.py
+++ Air-Glide(WebApp)/app.py
@@ -5,6 +5,8 @@ import numpy as np
 import face_recognition
 import time
 import pyautogui
+import HANDDETECTION as hdt
+from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 cap=cv2.VideoCapture(0)
@@ -96,6 +98,86 @@ def generate_frame_login():
         yield(b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
 
+def generate_frames_virtual_mouse():
+    hweb = 480
+    wweb = 640
+    cap.set(3,wweb)
+    cap.set(4,hweb)
+    pTime = 0
+    wscr , hscr = pyautogui.size()
+    tym = 0
+    pixels = 100
+    
+    detector = hdt.HandDetector()
+    while True:
+        frame=camera()
+        frame = cv2.flip(frame,1)
+        frame = detector.findHands(frame)
+        lmlist = detector.findPosition(frame)
+
+        #tip of all the fingers
+
+        if len(lmlist) != 0:
+            x0,y0 = lmlist[4][1:]
+            x1, y1 = lmlist[8][1:]
+            x2, y2 = lmlist[12][1:]
+            x3, y3 = lmlist[16][1:]
+            x4, y4 = lmlist[20][1:]
+
+            fingers = detector.fingersup()
+
+
+            cv2.rectangle(frame,(pixels,pixels),(wweb - pixels, hweb - pixels),(255,255,0),2)
+
+
+            #only index finger moving mode:
+            if fingers == [0,1,0,0,0]:
+                xc = np.interp(x1,(pixels,wweb - pixels),(0,wscr))
+                yc = np.interp(y1, (pixels, hweb - pixels), (0, hscr))
+
+                pyautogui.moveTo(xc,yc)
+                cv2.circle(frame,(x1,y1),15,(140,140,0),cv2.FILLED)
+
+            #for clicking: middle and index fing
+            if fingers == [0,1,1,0,0]:
+
+                lenght, frame  = detector.distance(8,12,frame)
+                if lenght <40:
+                    tym+=1
+                    if tym>30:
+                        cv2.circle(frame,(x1,y1),15,(0,255,0),cv2.FILLED)
+                        pyautogui.click()
+                        tym = 0
+
+            #thumb up for win r
+            if fingers == [1,0,0,0,0]:
+                tym+=1
+                if tym > 40:
+                    cv2.circle(frame, (x0, y0), 15, (0, 255, 0), cv2.FILLED)
+                    pyautogui.hotkey("win","r")
+                    tym = 0
+
+
+            #for closing opencv
+            if fingers == [0,0,0,0,1]:
+                tym+=1
+                if tym > 40:
+                    cv2.circle(frame, (x4, y4), 15, (0, 255, 0), cv2.FILLED)
+                    pyautogui.hotkey("q")
+                    tym = 0
+
+        #frame rate
+        cTime = time.time()
+        fps = 1 / (cTime - pTime)
+        pTime = cTime
+
+        cv2.putText(frame, str(int(fps)), (10, 70), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 0), 3)
+        ret,buffer=cv2.imencode('.jpg',frame)
+        frame=buffer.tobytes()
+        yield(b'--frame\r\n'
+                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+    
+
 
 @app.route('/')
 def home():
@@ -125,6 +207,10 @@ def video_signup():
 def video_login():
     return Response(generate_frame_login(),mimetype='multipart/x-mixed-replace; boundary=frame')
 
+@app.route('/video_virtual_mouse')
+def video_virtual_mouse():
+    return Response(generate_frames_virtual_mouse(),mimetype='multipart/x-mixed-replace; boundary=frame')
+
 @app.route('/capture')
 def capture():
     while True:
@@ -132,7 +218,7 @@ def capture():
         path='E:\Air-Glide(WebApp) not gitHub\Images'
         cv2.imwrite(os.path.join(path,"Anirudh.png"), frame)
         break
-    return redirect(url_for('home'))
+    return redirect(url_for("login"))
     
     
     
